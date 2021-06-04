22e317070333c7646e639554c01719c3232ed316
Emil Bahshiev <emil.bakhshiev@yandex.ru>
2021-06-04 12:11:31 +0300

    feat: create movies controllers

---
+++ controllers/movie.js
@@ -0,0 +1,82 @@
+const Movie = require('../models/movie');
+const AuthError = require('../errors/AuthError');
+const NotFoundError = require('../errors/NotFoundError');
+const ValidationError = require('../errors/ValidationError');
+const DuplicateError = require('../errors/DuplicateError');
+
+const getMovies = (req, res, next) => {
+    Movie.find({})
+      .then((cards) => res.status(200).send(cards))
+      .catch(next);
+  };
+
+const createMovie = (req, res, next) => {
+    const owner = req.user._id;
+    const {
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailer,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+    } = req.body;
+  
+    Movie.create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailer,
+      nameRU,
+      nameEN,
+      thumbnail,
+      movieId,
+      owner,
+    })
+      .then((movie) => res.send(movie))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          next(new ValidationError('Неверные данные'));
+        } else {
+          next(err);
+        }
+      });
+  };
+
+  const deleteMovie = (req, res, next) => {
+    const userId = req.user._id;
+    Movie.findById(req.params.movieId)
+      .orFail(() => { throw new NotFoundError('NotFound'); })
+      .then((movie) => {
+        if (movie.owner.toString() !== userId) {
+          throw new ForbiddenError(
+            'Действие недоступно. Вы не являетесь владельцем фильма',
+          );
+        }
+        Movie.findByIdAndDelete(req.params.movieId)
+          .then((movie) => res.send(movie))
+          .catch(next);
+      })
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          next(new ValidationError('Неверные данные'));
+        } else if (err.statusCode === 404) {
+          next(new NotFoundError('Фильм не найден'));
+        } else {
+          next(err);
+        }
+      });
+  };
+
+  module.exports = {
+    getMovies,
+    createMovie,
+    deleteMovie
+  }
